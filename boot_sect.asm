993a9c082b8b8cdc7eb840f341b094e6099f4c79
Elias <elias@eliasschulze.se>
2016-11-09 10:26:07 +0100

    Hello message

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,13 +1,26 @@
 ;
-; A simple boot sector program that loops forver.
+; A simple boot sector that invokes 0x10 "Video Services" interrupt
+; with ah set to 0x0e "Write Character in TTY Mode".
 ;
 
-loop:
-    
-  jmp loop
+mov ah, 0x0e
 
-; Pad with 0 until 510th byte
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'o'
+int 0x10
+
+jmp $ ; Hang
+
+;
+; Padding and magic number.
+;
+
 times 510-($-$$) db 0
-
-; Last two bytes is the magic number
 dw 0xaa55
